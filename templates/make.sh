762eb77188ecab43d863f2bd254f81fc9426c8f4
alex <alex@slab.org>
2014-12-09 15:46:14 +0000

    ready for alpha..

--- templates/make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-pandoc --variable mainfont="Linux Libertine O" --latex-engine=xelatex  -V geometry:margin=1in --bibliography=references.bib template.md -o template.pdf
-#pandoc --variable mainfont="Linux Libertine O" --latex-engine=xelatex  -V geometry:margin=1in --bibliography=references.bib --reference-odt=iclc2015-reference.odt template.md -o template.odt
-#pandoc -N -V geometry:margin=2cm --bibliography=references.bib template.md -o template.html
-#pandoc -N -V geometry:margin=2cm --bibliography=references.bib template.md -o template.docx
